65789f0bcd8100eb244de299c596b0bf38b60b24
xtrmdarc <diegoreyesco@hotmail.com>
2020-08-25 17:02:06 -0500

    Add and style basic add measurement page

--- src/containers/AddMeasure.js
+++ src/containers/AddMeasure.js
@@ -2,13 +2,27 @@ import React from 'react';
 
 class AddMeasure extends React.Component {
   render() { 
+    let date = new Date()
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    console.log(`${day}/${month}/${year}`);
     return ( 
       <div className="addMeasure">
-        <div>
-          Measure your expense
+        <div className="pageHeader">
+          <h2 className="pageTitle">Utility expense</h2>
         </div>
         <div className="mainContent">
-          
+          <input className="valueInput" type="text" placeholder="$0.00"/>
+          <label htmlFor="date">
+            Date
+            <input type="date" />
+          </label>
+          <button className="cta">
+            Add
+          </button>
         </div>
       </div>
     );
