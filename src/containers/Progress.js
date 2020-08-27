a5811058d4df75d7f80a0c6da9f472abe90e5cc1
xtrmdarc <diegoreyesco@hotmail.com>
2020-08-27 00:37:20 -0500

    Add styling to progress date list titles

--- src/containers/Progress.js
+++ src/containers/Progress.js
@@ -21,6 +21,7 @@ class Progress extends React.Component {
     progress.forEach(measure => {
       if(!dailyProgress[measure.date])
         dailyProgress[measure.date] = {
+          date: measure.date,
           totalAmount: measure.value,
           measurements: [measure],
         };
@@ -30,14 +31,37 @@ class Progress extends React.Component {
       }
     });
     const renderList = [];
+    const orderedProgress = (Object.entries(dailyProgress)).sort((a,b) => new Date(b[0]).getTime() - new Date(a[0]).getTime());
+    //get todays progress
+    const todayProgress = [];
+    const pastProgress = [];
+    const futureProgress = [];
+    const today = new Date(Date.now());
+    // todayProgress.push(orderedProgress.filter(p => new Date(p.date).toDateString === today));
+    // get past progress
 
-    for(const [key, value] of (Object.entries(dailyProgress)).sort((a,b) => new Date(b[0]).getTime() - new Date(a[0]).getTime())) {
-      renderList.push(<ProgressItem progressData={value} progressDate={key}/>)
+    
+    for(const [key, value] of orderedProgress) {
+      const progressItem = <ProgressItem progressData={value} progressDate={key} />;
+      const progressItemDate = new Date(value.date);
+      if(progressItemDate.toDateString() === today.toDateString()) {
+        todayProgress.push(progressItem);
+      }
+      else if(progressItemDate.getTime() < today.getTime() ){
+        pastProgress.push(progressItem);
+      }
+      else  futureProgress.push(progressItem);
+
+      // renderList.push(<ProgressItem progressData={value} progressDate={key}/>)
     }
-
     return (  
       <div className="progress">
-        {renderList}
+        <h4 className="titleList">Future</h4>
+        {futureProgress}
+        <h4 className="titleList">Today</h4>
+        {todayProgress}
+        <h4 className="titleList">Past</h4>
+        {pastProgress}
       </div>
     );
   }
