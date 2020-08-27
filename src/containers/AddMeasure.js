58a3621f3b738bb2d88e0d4b12c2d2610901ff42
xtrmdarc <diegoreyesco@hotmail.com>
2020-08-27 15:44:27 -0500

    Validate user input on add measure

--- src/containers/AddMeasure.js
+++ src/containers/AddMeasure.js
@@ -10,6 +10,7 @@ class AddMeasure extends React.Component {
     this.state = {
       amountValue: '',
       dateValue: '',
+      errorSubmission: '',
     }
     this.props.changeHeader('Add measurement', 2);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -26,8 +27,28 @@ class AddMeasure extends React.Component {
   }
 
   handleChange(e) {
-    const val = e.target.value;
+    let val = e.target.value;
     const elId = e.target.id;
+
+    if(elId === 'amountValue') {
+      val = e.target.value.replace(/[^(\d.)]/g, '');
+      let firstDotFound = false;
+      const valArr = val.split('');
+      val = '';
+      valArr.forEach( c => {
+        if(firstDotFound) {
+          if(c != '.')
+            val += c;
+        }
+        else {
+          if(c === '.') {
+            firstDotFound = true;
+          }
+          val += c;
+        }
+      });
+    }
+
     this.setState({
       [elId]: val,
     });
@@ -35,33 +56,51 @@ class AddMeasure extends React.Component {
 
   handleSubmit() {
     const { user, measureItem, loadProgress } = this.props;
+    const { amountValue, dateValue } = this.state;
+    if ( amountValue === '0.00' || !parseFloat(this.state.amountValue))
+    {
+      this.setState({
+        errorSubmission: "Expense amount can't be blank",
+      });
+      return;
+    }
+    if( dateValue === '' ) {
+      this.setState({
+        errorSubmission: 'Specify a date before submitting',
+      });
+      return;
+    }
 
     const measurementObj = {
-      value: parseFloat(this.state.amountValue),
+      value: parseFloat(this.state.amountValue) ? parseFloat(this.state.amountValue) : 0.00,
       date: this.state.dateValue,
       user_id: user.id,
       ex_cat_id: measureItem.id,
     };
+
     expensifyApi.createNewMeasurement(measurementObj)
     .then(p => {
       loadProgress();
     })
     .catch(e => console.log(e));
+    this.props.history.push('/');
   }
 
   render() { 
     const {measureItem}  = this.props;
+    const { errorSubmission, amountValue } = this.state;
     return ( 
       <div className="addMeasure">
         <div className="pageHeader">
           <h2 className="pageTitle">{measureItem.name} expense</h2>
         </div>
         <div className="mainContent">
-          <input id="amountValue" className="valueInput" type="text" placeholder="$0.00" onChange={this.handleChange} value={this.state.valueInput}/>
+          <input id="amountValue" className="valueInput" type="text" placeholder="$0.00" onChange={this.handleChange} value={amountValue}/>
           <label htmlFor="date">
             Date
             <input id="dateValue" type="date" onChange={this.handleChange} />
           </label>
+          {errorSubmission !== '' ? <span className="errorSubmission">{errorSubmission}</span> : ''}
           <button className="cta" onClick={this.handleSubmit} value={this.state.dateValue}>
             Add
           </button>
