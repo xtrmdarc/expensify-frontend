88b496528698d6db3ed3ac495c568c501749410f
xtrmdarc <diegoreyesco@hotmail.com>
2020-08-27 12:41:23 -0500

    Finish footer styling

--- src/components/FooterNav.js
+++ src/components/FooterNav.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import addIcon from '../assets/img/add.svg';
+import growIcon from '../assets/img/grow.svg';
 const FooterNav = props => {
   const { actualTab, userId } = props;
   console.log(props);
   return (
     <footer>
       <nav>
-        <Link to="/" className={`navItem ${actualTab === 'list' ? 'active' : ''}`}>Add expense</Link>
-        <Link to={`/progress/${userId}`} className={`navItem ${actualTab === 'progress' ? 'active' : ''}`}>Your progress</Link>
+        <Link to="/" className={`navItem ${actualTab === 'list' ? 'active' : ''}`}>
+          <img src={addIcon} />
+          <span>Add expense</span>
+        </Link>
+        <Link to={`/progress/${userId}`} className={`navItem ${actualTab === 'progress' ? 'active' : ''}`}>
+          <img src={growIcon} />
+          <span>Track it</span>
+        </Link>
       </nav>
     </footer>
   )
