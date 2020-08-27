c32dab2c780e893637a146468cba3e86903ab937
xtrmdarc <diegoreyesco@hotmail.com>
2020-08-27 16:29:47 -0500

    Add styles to category item arrow

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import TestImage from '../assets/img/test.svg';
+import arrowIcon from '../assets/img/right_arrow.svg';
 
 const CategoryItem = props => {
   const { name, id } = props.categoryData;
 
   return (
     <Link to={`/expense/${id}`} href="#" className="categoryItem">
-      <img src={TestImage} alt=""/>
+      <img className="categoryImg" src={TestImage} alt=""/>
       <span>{name}</span>
+      <img className="arrowIcon" src={arrowIcon}/>
     </Link>
   );
 }
