d7c0750fb53825acd7de49a69e7d1a2303b1919d
xtrmdarc <diegoreyesco@hotmail.com>
2020-09-04 19:56:36 -0500

    Redirect proxy to production server

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3001',
-      // target: 'https://xtrmdarc-expensify.herokuapp.com/',
+      target: 'https://xtrmdarc-expensify.herokuapp.com/',
       changeOrigin: true,
       pathRewrite: {
         '^/api': '/',
